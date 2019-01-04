e179a0fb751cef0bb0b224b32d77249b4e624d37
uriochanyasu <uriochan777@gmail.com>
2019-01-04 13:07:30 +0000

    kadai10

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:destroy]
   
-  def index
-    @tasks = Task.all.page(params[:page]).per(3)
-  end  
-  
-  def show
-  end 
-  
-  def new
-    @task = Task.new
-  end 
+ 
   
   def create
     @task = current_user.tasks.build(task_params)
@@ -28,19 +19,7 @@ class TasksController < ApplicationController
     
   end 
   
-  def edit
-  end 
   
-  def update
-    if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
-    end  
-    
-  end 
   
   
   def destroy
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @task = current_user.tasks.build  # form_for 用
+      @task = current_user.tasks.build  
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
     end
   end
